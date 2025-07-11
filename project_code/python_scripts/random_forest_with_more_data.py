453fa75fc31fdf2ec74a799ed705819c7df24506
sara meira and efraim <sarameirab@gmail.com>
2025-07-11 13:10:08 +0300

    propensity score analysis

--- project_code/python_scripts/random_forest_with_more_data.py
+++ project_code/python_scripts/random_forest_with_more_data.py
@@ -1,5 +1,5 @@
 from project_code.python_scripts.urls import download_google_drive_url, DEPMAP_MUTATION_DATA, PRISM_DRUG_REPURPOSING, \
-    DEPMAP_MODEL_MAPPING
+    DEPMAP_MODEL_MAPPING, download_url, DEPMAP_EXPRESSION
 from random_forest_on_drug import run_random_forest_on_multiple_drugs_and_write_results_to_csv
 import pandas as pd
 lof_path = download_google_drive_url(DEPMAP_MUTATION_DATA)
@@ -22,14 +22,31 @@ model_df = model_df[model_df['cell_line'].isin(available_cell_lines)]
 columns_to_keep = ['cell_line', 'DepmapModelType', 'OncotreePrimaryDisease', 'OncotreeSubtype', 'OncotreeCode', 'Sex', 'PrimaryOrMetastasis', 'OncotreeLineage', 'GrowthPattern', 'SampleCollectionSite']
 model_df = model_df[columns_to_keep]
 
+expression_path =download_google_drive_url(DEPMAP_EXPRESSION)
+expression_df = pd.read_csv(expression_path)
+expression_df = expression_df.rename(columns={'Unnamed: 0': 'ProfileID'})
+
+expression_mapping_path = download_url("https://42basepairs.com/download/s3/d3b-openaccess-us-east-1-prd-pbta/pbta-splicing/v8/OmicsDefaultModelProfiles.csv")
+expression_mapping_df = pd.read_csv(expression_mapping_path)
+expression_mapping_df = expression_mapping_df.rename(columns={'Unnamed: 0': 'ProfileID'})
+expression_merge = pd.merge(expression_mapping_df, expression_df,
+                          left_on="ProfileID",
+                          right_on="ProfileID",
+                          how='inner')
+expression_merge = expression_merge.rename(columns={'ModelID': 'cell_line'})
+expression_merge = expression_merge.drop(['ProfileType', 'ProfileID'], axis=1)
+
+
 feature_df = pd.merge(model_df, lof_df, on='cell_line', how='inner')
+feature_df = pd.merge(feature_df, expression_merge, on='cell_line', how='inner')
+
 merged_df = pd.merge(feature_df, prism_df, on='cell_line', how='inner')
 
 import pandas as pd
 random_forest_results = pd.read_csv('random_forest_results.csv')
 
 random_forest_results_sorted = random_forest_results.sort_values(by="pearson_correlation", ascending=False)
-top_5_drugs = random_forest_results_sorted.head(5)
+top_5_drugs = random_forest_results_sorted.head(20)
 
 print("Running random forest on " +str(top_5_drugs))
 
